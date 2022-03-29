6002ba6e7de9049c7075f4f0cedb8f0631749956
Henrique Oliveira <henrique.olive29@gmail.com>
2022-03-29 18:39:05 +0100

    editbookDTO

--- library.ui/src/models/books/EditBookDTO.ts
+++ library.ui/src/models/books/EditBookDTO.ts
@@ -1,13 +1,8 @@
 export class EditBookDTO{
     Id: number  = 0;
-
     Name: string = "";
-
     Price: number = 0 ;
-
     Description: string = "";
-
     StockNumber: number = 0;
-
     AuthorId : number = 0;
 }
